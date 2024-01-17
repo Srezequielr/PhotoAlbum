77c8c69c03d75d14596a0cce786acb58cd8fef92
Srezequielr <julianrierawrk@gmail.com>
2024-01-17 19:11:53 -0300

    Link to photographer added

--- photo-album/components/Masonry/Masonry.jsx
+++ photo-album/components/Masonry/Masonry.jsx
@@ -1,17 +1,35 @@
 import { Masonry } from "@mui/lab";
 import styles from "./Masonry.module.css";
 import { createClient } from "pexels";
+import Link from "next/link";
 const APIKEY = process.env.NEXT_PUBLIC_APIKEY;
 
 const client = createClient(APIKEY);
 
 export default async function MasonryCom() {
-  const photos = await client.photos.curated({ page: 1, per_page: 30 }).then(response => response.photos);
+  const photos = await client.photos
+    .curated({ page: 1, per_page: 30 })
+    .then((response) => response.photos);
+
+  console.log(photos);
   return (
     <section className={styles.masonryContainer}>
       <Masonry className={styles.masonry} columns={2} spacing={2}>
         {photos?.map((photo, index) => {
-          return <img className={styles.images} src={photo.src.original} key={photo.id} alt={photo.alt}/>;
+          return (
+            <div className={styles.imgContainer}>
+              <img
+                className={styles.images}
+                src={photo.src.original}
+                key={photo.id}
+                alt={photo.alt}
+              />
+              <p className={styles.credits}>
+                Photography by{" "}
+                <Link className={styles.link} href={photo.photographer_url}>{photo.photographer}</Link>
+              </p>
+            </div>
+          );
         })}
       </Masonry>
     </section>
